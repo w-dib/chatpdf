7e585f83081c6d29e58b0443c3f18c31dd074509
w-dib <wdanieldib@gmail.com>
2023-09-19 17:13:49 +0400

    Add clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Hello world</div>;
+  return <Button>Click me</Button>;
 }
