75d8d2c8a2b5778974d5e57c9932d7d23c585858
w-dib <wdanieldib@gmail.com>
2023-09-19 17:16:45 +0400

    Add background

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
-export default function Home() {
-  return <Button>Click me</Button>;
+export default async function Home() {
+  return (
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <h1></h1>
+    </div>
+  );
 }
