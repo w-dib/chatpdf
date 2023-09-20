f9bc181343e57a43fe8cc610d5cedcb2d1a6032b
w-dib <wdanieldib@gmail.com>
2023-09-20 15:39:49 +0400

    Fix styles, add methods

--- components/fileUpload.tsx
+++ components/fileUpload.tsx
@@ -3,14 +3,20 @@ import { Inbox } from "lucide-react";
 import { useDropzone } from "react-dropzone";
 
 const FileUpload = () => {
-  const { getRootProps, getInputProps } = useDropzone();
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: { "application/pdf": [".pdf"] },
+    maxFiles: 1,
+    onDrop: (acceptedFiles) => {
+      console.log(acceptedFiles);
+    },
+  });
 
   return (
     <div className="p-2 bg-white rounded-xl">
       <div
         {...getRootProps({
           className:
-            "border-dashed boorder-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col",
+            "border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col",
         })}
       >
         <input {...getInputProps()} />
